365a352374ac26514a44434e614cc545d4a495cc
dashesy <dash.back.slash@gmail.com>
2022-10-22 13:01:14 -0700

    featurize

--- imagesorter/sorter.py
+++ imagesorter/sorter.py
@@ -6,6 +6,8 @@ import os.path as op
 from imagesorter.resnet18_featurizer import Resnet18Featurizer as Featurizer
 
 def featurize(srcdir: str):
+    """Featurize all the images in the path
+    """
     featurizer = Featurizer().eval()
     features = {}
     with torch.no_grad():
@@ -15,7 +17,25 @@ def featurize(srcdir: str):
     return features
 
 def sort(srcdir: str) -> list[str]:
+    """Sort all the images in the path and return the sorted names
+    """
     features = featurize(srcdir)
-    sorted = []
     sim = nn.CosineSimilarity(eps=1e-6, dim=0)
-
+    sorted = []
+    sims = {}
+    for src, feat in features.items():
+        if len(sorted) < 2:
+            # add the first two
+            sorted.append(src)
+            continue
+        # got through previously sorted, and find the top-2 most similar to insert
+        max_sim = None
+        max_idx = None
+        for ii, prev_src in enumerate(sorted):
+            s = sim(feat, features[prev_src])
+            sims[(src, prev_src)] = sims[(prev_src, src)] = s
+            if max_sim is None or s > max_sim:
+                max_sim = s
+                max_idx = ii
+    
+    return sorted
