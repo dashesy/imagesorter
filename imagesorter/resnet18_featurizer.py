35c58f3508be489479b419dc39eeb1326824938b
dashesy <dashesy@gmail.com>
2022-10-23 17:58:10 -0700

    Add vit_b_16 and option to select featurizer

--- imagesorter/resnet18_featurizer.py
+++ imagesorter/resnet18_featurizer.py
@@ -3,11 +3,12 @@ from torchvision.models.feature_extraction import create_feature_extractor
 from imagesorter.featurizer import Featurizer
 
 class Resnet18Featurizer(Featurizer):
-    def __init__(self, size: int = 112):
+    def __init__(self, size: int = 224, feat_name: str = None):
         super().__init__(size)
         model = models.resnet18(pretrained=True)
-        self.featurizer = create_feature_extractor(model, return_nodes=['avgpool'])
+        self.feat_name = feat_name or "avgpool"
+        self.featurizer = create_feature_extractor(model, return_nodes=[self.feat_name])
     
     def forward(self, path: str):
         img = super().forward(path)
-        return self.featurizer(img)["avgpool"].squeeze()
+        return self.featurizer(img)[self.feat_name].flatten()
